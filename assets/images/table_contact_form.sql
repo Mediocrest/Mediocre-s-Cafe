198a6bb2c89bebb0f34908a9e0e7dbf657690336
Mediocrest <floresalbion@gmail.com>
2023-02-08 13:11:22 +0800

    combined all files and added FAQ

---
+++ assets/images/table_contact_form.sql
@@ -0,0 +1,42 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3307
+-- Generation Time: Feb 26, 2022 at 10:09 AM
+-- Server version: 10.4.13-MariaDB
+-- PHP Version: 7.3.21
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `contactinformation`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `table_contact_form`
+--
+
+DROP TABLE IF EXISTS `table_contact_form`;
+CREATE TABLE IF NOT EXISTS `table_contact_form` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `message` longtext NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ assets/php.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <body>
+
+    <?php ?>
+
+    <form action="" method="get">
+        Name: <input type="text" name="name">
+        <input type="submit">
+    </form>
+
+    </body>
+</html>
